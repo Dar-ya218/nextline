3c9a9d57dfe66a8cca0bfaf347c29ecb7ee9b623
Dar-ya218 <dasharoura13465@gmail.com>
2024-03-31 09:44:21 +0200

    get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -12,15 +12,29 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# include <unistd.h>
 # include <stdlib.h>
 # include <fcntl.h>
-# include <unistd.h>>
 # include <stdio.h>
 
-//# ifndef BUFFER_SIZE
-//#  define BUFFER_SIZE 42
-//# endif
+typedef struct s_list
+{
+	char			*content;
+	struct s_list	*next;
+}	t_list;
 
 char	*get_next_line(int fd);
-size_t	ft_strlen(const char *s);
-char	*ft_strdup(const char *s1);
+t_list	*read_to_node(int fd, int *byte_read);
+int		find_newline(t_list *node);
+void	create_list(t_list **list, int fd);
+void	list_to_line(t_list *byte_list, char **line);
+void	create_line(t_list **byte_buff, char **line, int i);
+void	clean_list(t_list **list);
+void	pass_over_node(t_list *list, t_list **pass_over);
+void	free_list(t_list *list);
+
+#endif
